8b38c79016e6e837ff4787c83b4827e4faa445b4
MMurph11 <57727495+MMurph11@users.noreply.github.com>
2022-11-03 15:17:40 +0000

    Add files via upload

--- player_similarity_streamlit.py
+++ player_similarity_streamlit.py
@@ -27,12 +27,8 @@ st.markdown('Data: Wyscout | Leagues: Top 5 European Mens Leagues | Seasons: 202
 # Read both excel files
 @st.cache(suppress_st_warning=True)
 def getData():
-    username = 'MMurph11'
-    token = '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'
-    github_session = requests.Session()
-    github_session.auth = (username, token)
-    similarity_url = 'https://raw.githubusercontent.com/MMurph11/player_similarity/main/similarity_df.csv?token=GHSAT0AAAAAAB2TQVBIAJ4K23NGPYMMKIQEY3D24IA'
-    metrics_url = 'https://raw.githubusercontent.com/MMurph11/player_similarity/main/metrics_df.csv?token=GHSAT0AAAAAAB2TQVBIHC2ADU2CCA7DZNK4Y3D247A'
+    similarity_url = 'https://raw.githubusercontent.com/MMurph11/player_similarity/main/similarity_df.csv'
+    metrics_url = 'https://raw.githubusercontent.com/MMurph11/player_similarity/main/metrics_df.csv'
     similarity_download = requests.get(similarity_url).content
     metrics_download = requests.get(metrics_url).content
     similarity_df = pd.read_csv(io.StringIO(similarity_download.decode('utf-8')))
@@ -49,7 +45,7 @@ players = [x for x in players if str(x) != 'nan']
 
 # Create dropdown that filters the data
 player_select = st.sidebar.selectbox('Select Player', players)
-player_df = similarity_df[['Player','Team within selected timeframe','Position1','Age',player_select]].sort_values(player_select, ascending=False)
+player_df = similarity_df[["Player","Team within selected timeframe","Position1","Age",player_select]].sort_values(player_select, ascending=False)
 
 # Create team filter
 teams = player_df['Team within selected timeframe'].unique()
