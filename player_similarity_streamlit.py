283b46d918fbf5eb93b8b1581f05c6aa95df8ca5
MMurph11 <57727495+MMurph11@users.noreply.github.com>
2022-11-04 14:47:13 +0000

    Add files via upload

--- player_similarity_streamlit.py
+++ player_similarity_streamlit.py
@@ -47,6 +47,10 @@ players = [x for x in players if str(x) != 'nan']
 player_select = st.sidebar.selectbox('Select Player', players)
 player_df = similarity_df[["Player","Team within selected timeframe","Position1","Age",player_select]].sort_values(player_select, ascending=False)
 
+players_team = player_df.loc[player_df['Player']==player_select]
+team_select = st.sidebar.selectbox('Select Team', players_team['Team within selected timeframe'].unique())
+player_df = similarity_df[["Player",team_select,"Position1","Age",player_select]].sort_values(player_select, ascending=False)
+
 # Create team filter
 teams = player_df['Team within selected timeframe'].unique()
 teams = np.sort(teams)
